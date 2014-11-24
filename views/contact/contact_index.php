75e723f1ebc98ce140a391f8bd344f953981f55f
eve-link <eve.link.1992@gmail.com>
2014-11-24 10:26:59 +0200

    update

--- views/contact/contact_index.php
+++ views/contact/contact_index.php
@@ -1,53 +1,9 @@
 <section class="text-content">
     <div class="content-wrap">
         <div class="content-formatted cfx" >
-            <h1>This is where your products live.</h1>
+            <h1>Kontakt</h1>
             <p>Each company has its own needs on the basis of which we develop a tailor-made service package. Our strength is our well-trained and experienced staff, which is key to maintain international client relationships. We offer our clients a professional and personal service in all accountancy-related questions.<p>
-            <div class="text-large-img"><img src="../photos/oslo.png" alt="Tootepilt"></div>
-
-            <p>
-                The outsourcing of business administration services also gives the company an additional means of internal control and monitoring. By establishing SIA Leinonen Audit in 2005 we have expanded our services and now we are also able to provide: <br><br>
-                °  Monthly, quarterly or annual reporting in local or number of foreign languages. The number and choice of languages depend on the branch office. <br><br>
-                °  Preparing of financial statements in accordance with local statutory regulations and IAS.<br><br>
-                All entrepreneurs engaging in business, farming, forestry or any other income-generating activity, under all legal forms, are entered in the prepayment register on the basis of the facts submitted in the Start-up notification.</p>
         </div>
-    </div>
-    <div class="table-content-wrap">
-        <h1>Accounting references</h1>
-        <table>
-            <tr>
-                <td>Country</td>
-                <td>Capital</td>
-                <td>Country</td>
-                <td>Capital</td>
-                <td>Country</td>
-                <td>Capital</td>
-            </tr>
-            <tr>
-                <td>Finland</td>
-                <td>Helsinki</td>
-                <td>Finland</td>
-                <td>Helsinki</td>
-                <td>Finland</td>
-                <td>Helsinki</td>
-            </tr>
-            <tr>
-                <td>Estonia</td>
-                <td>Tallinn</td>
-                <td>Estonia</td>
-                <td>Tallinn</td>
-                <td>Estonia</td>
-                <td>Tallinn</td>
-            </tr>
-            <tr>
-                <td>Latvia</td>
-                <td>Riga</td>
-                <td>Latvia</td>
-                <td>Riga</td>
-                <td>Latvia</td>
-                <td>Riga</td>
-            </tr>
-        </table>
     </div>
 
     <fieldset>
