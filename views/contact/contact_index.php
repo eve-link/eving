516f0d11ef6c7ddfbc335525be725dde4f3bc60a
Ingrid <Kelder>
2014-11-26 12:53:32 +0200

    Remove country section

--- views/contact/contact_index.php
+++ views/contact/contact_index.php
@@ -5,43 +5,6 @@
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pretium et ex at ultricies. Pellentesque posuere fermentum mollis. Fusce sed imperdiet dui. Aenean luctus maximus dictum. Integer massa ex, vulputate eu suscipit sit amet, aliquam eget erat. Nulla aliquam, sem ac porta semper, mauris enim consequat felis, ac gravida justo felis in nisl. Mauris ullamcorper tempor neque vitae ullamcorper. Phasellus mauris mi, elementum malesuada egestas luctus, ornare quis augue. Proin pulvinar mauris id varius molestie. Suspendisse varius velit eu lacus ornare venenatis. Morbi diam sapien, facilisis eu elit vitae, pulvinar faucibus felis. Fusce dapibus erat sed luctus accumsan.<p>
         </div>
     </div>
-    <div class="table-content-wrap">
-        <h1>Accounting references</h1>
-        <table>
-            <tr>
-                <td>Country</td>
-                <td>Capital</td>
-                <td>Country</td>
-                <td>Capital</td>
-                <td>Country</td>
-                <td>Capital</td>
-            </tr>
-            <tr>
-                <td>Finland</td>
-                <td>Helsinki</td>
-                <td>Finland</td>
-                <td>Helsinki</td>
-                <td>Finland</td>
-                <td>Helsinki</td>
-            </tr>
-            <tr>
-                <td>Estonia</td>
-                <td>Tallinn</td>
-                <td>Estonia</td>
-                <td>Tallinn</td>
-                <td>Estonia</td>
-                <td>Tallinn</td>
-            </tr>
-            <tr>
-                <td>Latvia</td>
-                <td>Riga</td>
-                <td>Latvia</td>
-                <td>Riga</td>
-                <td>Latvia</td>
-                <td>Riga</td>
-            </tr>
-        </table>
-    </div>
 
     <fieldset>
         <div class="contact">
